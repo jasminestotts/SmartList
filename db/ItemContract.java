1a1915ee3e4ee429e3dc02735c6bfb1e6a2bd837
jasminestotts <jasminestotts@gmail.com>
2018-07-29 22:20:51 -0700

    first commit

--- db/ItemContract.java
+++ db/ItemContract.java
@@ -6,6 +6,7 @@ public class ItemContract {
     public static final String DB_NAME = "com.gmail.jasminestotts.smartlist.db";
     public static final int DB_VERSION = 1;
 
+    //used to create columns
     public class ItemEntry implements BaseColumns {
         public static final String TABLE = "items";
         public static final String COL_ITEM_TITLE = "title";
